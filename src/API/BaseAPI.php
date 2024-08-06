a3daa8dd12f4b9db9e015654d35631028933afb3
Clairmont RAJAONARISON <clairmont.rajaonarison@gmail.com>
2024-08-06 16:05:58 +0300

    feat : add BaseAPI

---
+++ src/API/BaseAPI.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Koders\EstimationModule\API;
+
+use Exception;
+use Illuminate\Support\Facades\Http;
+
+abstract class BaseAPI
+{
+    /**
+     * @var array
+     */
+    const ALLOWED_ENDPOINT = [
+        '/dispositions/all',
+        '/geocode/',
+        '/dispositions/intersects',
+        '/dispositions/'
+    ];
+
+    /**
+     * @var string baseUrl
+     */
+
+    const BASE_URL = "https://api.sogefi-sig.com/";
+
+    /**
+     * @var string
+     */
+    const ALL_DISPOSITION_ENDPOINT = '/dispositions/all';
+
+    /**
+     * @var string
+     */
+    const UNIQUE_DISPOSITION_ENDPOINT = '/dispositions/';
+
+    /**
+     * For all get methods in the API Request
+     */
+
+    public static function get(string $endpoint, array $query = [], ?bool $withParams = false)
+    {
+        $endpointToValidate = $withParams ? "/" . explode("/", $endpoint)[1] . "/" : $endpoint;
+        if (BaseAPI::validateRoute($endpointToValidate)) {
+            $endpoint = BaseAPI::generateUrl($endpoint);
+            try {
+                return Http::get($endpoint, $query);
+            } catch (Exception $e) {
+                throw new Exception($e->getMessage(), $e->getCode());
+            }
+        }
+        throw new Exception("The endpoint {$endpoint} is not allowed");
+    }
+
+
+    /**
+     * Validate the routes to be in the allowed endpoint
+     * @param string $endpoint
+     * @return bool true if allowed,  false otherwise
+     */
+
+    private static function validateRoute(string $endpoint): bool
+    {
+        return in_array($endpoint, BaseAPI::ALLOWED_ENDPOINT);
+    }
+
+    /**
+     * Concatenate the baseURL and the endpoint
+     * @param string $endpoint
+     * @return string
+     */
+    private static function generateUrl(string $endpoint): string
+    {
+        return BaseAPI::BASE_URL . config('estimation-module.api_key') . "/dvf/v2" . $endpoint;
+    }
+}
